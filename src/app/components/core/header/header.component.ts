bac5de0bda435c5916abea1ac87b9e2ca4205d2e
Jacob Ambrose <jambrose@worldcubeassociation.org>
2023-10-06 21:23:20 -0400

    added nav menu

--- src/app/components/core/header/header.component.ts
+++ src/app/components/core/header/header.component.ts
@@ -7,10 +7,20 @@ import { Component, Input, OnInit, ViewChild } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   @Input() title: string = 'Southeast Cubing';
+  isNavActive = false;
+  transition = false;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleNav(toggled: boolean) {
+    this.isNavActive = toggled;
+    this.transition = true;
+    setTimeout(() => {
+      this.transition = false;
+    }, 500);
   }
 
 }
