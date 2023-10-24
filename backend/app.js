48b748ec5cbf72a639225fa88c4a77f4a7ab89c1
Jacob Ambrose <jambrose@worldcubeassociation.org>
2023-10-24 13:34:59 -0400

    added extra port

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,8 @@ app.use(cors());
 // Email mailer
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
-  port: 587,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
