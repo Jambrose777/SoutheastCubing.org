aa261ad336ce204c3699cd79df948b5f54a2576c
Jacob Ambrose <jambrose@worldcubeassociation.org>
2023-10-28 15:56:11 -0400

    added cool announcement banner

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -5,6 +5,8 @@ import { Colors } from 'src/app/shared/types';
 import { isMobile } from 'src/app/shared/functions';
 import { ContentfulEntryId } from 'src/app/models/Contentful';
 import { environment } from 'src/environments/environment';
+import { SubTopic } from 'src/app/models/SubTopic';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'se-home',
@@ -18,8 +20,9 @@ export class HomeComponent implements OnInit {
   description: string = '';
   photos: string[] = [];
   loadingContent: boolean = true;
+  subTopics: SubTopic[];
 
-  constructor(private contentful: ContentfulService, private themeService: ThemeService) { }
+  constructor(private contentful: ContentfulService, private themeService: ThemeService, private router: Router) { }
 
   ngOnInit(): void {
     // sets up main color for the home page
@@ -29,9 +32,19 @@ export class HomeComponent implements OnInit {
     this.contentful.getContentfulEntry(ContentfulEntryId.home).subscribe(res => {
       this.title = res.fields.title;
       this.description = res.fields.description;
+      this.subTopics = res.fields.subTopics.map(subTopic => ({ ...subTopic.fields, color: Colors[subTopic.fields.color] }))
       this.photos = res.fields.photos.map(photo => ({ path: photo.fields.file.url }));
       this.loadingContent = false;
     });
   }
 
+  announcementClick(subTopic: SubTopic) {
+    if(subTopic.buttonInternalLink) {
+      this.router.navigate([subTopic.buttonInternalLink])
+    } else if (subTopic.buttonExternalLink) {
+      this.router.navigate([]).then(() => {
+        window.open(subTopic.buttonExternalLink, '_blank');
+      });
+    }
+  }
 }
